250683d8e91d9c8a3d01e729bd39d1983a414a3a
Darshan Jain <109934911+1d2a3r4s5h6a7n@users.noreply.github.com>
2023-08-15 13:03:03 +0530

    Add files via upload

--- Program14.c
+++ Program14.c
@@ -1,28 +1,28 @@
 //C Program to Display Armstrong Numbers Between 1 to 1000
-//C Program to Check Armstrong Number
+
 #include<stdio.h>
 main()
 {
-	int n,sum=0,r,i,temp;
-	printf("Enter the Number:");
-	scanf("%d",&n);//153
-	temp=n; //temp=153(fixed)
-	while(n>0)  //for(temp=n;n!=0;n=n/10)
+	int num,sum,r,i,temp,start,end;
+	printf("Enter Start Number:");
+	scanf("%d",&start);
+	printf("Enter End Number:");
+	scanf("%d",&end);
+	for(i=start;i<=end;i++)
 	{
-		r=n%10;  //153/10=remainder(3) ===>r=15%10=remainder(5)===>r=1%10=remainder(1)
-		//r=3 ====> r=5====> r=1
-		sum=sum+(r*r*r);//sum=0+3*3*3==sum=27 ==>sum=27+5*5*5==sum=27+125=152  
-				//==>sum=152+1*1*1==sum=152+1=153
-		n=n/10;//n=153/10=quotient(15)==>n=15===>n=15/10=quotient(1)==>n=1
-					//===>n=1/10=quotient(0.1==0)==>n=0
-	}
-	if(sum==temp)  //153==153
-	{
-		printf("%d is Armstrong Number",sum);
-	}
-	else
-	{
-		printf("%d is not Armstrong Number",temp);
+		sum=0;
+		num=i; //num=1,2
+		temp=num; //temp=1,2
+		while(num>0)  //1>0,2>0
+		{
+			r=num%10; //r=1%10 ==> r=1,2
+			sum=sum+r*r*r; //sum=1,9
+			num=num/10; //num=0,0
+		}
+		if(sum==temp) //1==1 9==2
+		{
+			printf("%d \t",temp); //1
+		}
 	}
 	  
 }
